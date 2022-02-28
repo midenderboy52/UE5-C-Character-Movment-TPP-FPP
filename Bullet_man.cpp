db31b7f6ef7a2852b4fda8068ed5942334cc3d11
midenderboy52 <65211939+midenderboy52@users.noreply.github.com>
2022-02-28 14:54:20 +0600

    Update Bullet_man.cpp

--- Bullet_man.cpp
+++ Bullet_man.cpp
@@ -10,6 +10,8 @@ ABullet_man::ABullet_man()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	//Should also be a parent class of a blueprint actor class, to configure locations and rotations of components. 
 
 	bully = CreateDefaultSubobject<USphereComponent>(TEXT("bully"));
 	bully->InitSphereRadius(5.0f);
@@ -18,11 +20,14 @@ ABullet_man::ABullet_man()
 	RootComponent = bully;
 
 	move_bully = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("move_bully"));
+	//Updated component basically means TheMovementCompoenent is now the attached with bully thingy.
 	move_bully->UpdatedComponent = bully;
 	move_bully->InitialSpeed = 20000.0f;
 	move_bully->MaxSpeed = 20000.0f;
 	move_bully->bRotationFollowsVelocity = true;
 	move_bully->bShouldBounce = true;
+	
+	//If this bullet doesnt hit in 3 sec it gets destroyed
 	InitialLifeSpan = 3.0f;
 	
 
@@ -43,5 +48,5 @@ void ABullet_man::Tick(float DeltaTime)
 }
 
 void ABullet_man::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Hit){
-	
+	//Do whatever wanted
 }
