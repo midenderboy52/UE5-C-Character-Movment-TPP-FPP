c742e54467c8dbcb5e2e2bdc304ff82370d03bfa
midenderboy52 <65211939+midenderboy52@users.noreply.github.com>
2022-02-28 14:41:04 +0600

    Update Character_Movement.cpp

--- Character_Movement.cpp
+++ Character_Movement.cpp
@@ -1,56 +1,59 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PRO_IT_IS.h"
+#include "Character_Movement.h"
 #include "Camera/CameraComponent.h"
 #include "DrawDebugHelpers.h"
 #include "Bullet_man.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/InputComponent.h"
-#include "MyGameModeBase.h"
-#include "InputCoreTypes.h"
 
 
 
 // Sets default values
-APRO_IT_IS::APRO_IT_IS()
+ACharacter_Movement::ACharacter_Movement()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	//Possesing this player, you have to select Player 0 in unreal engine.
+	//Select tthis character after placing it in the level and click on it
+	//A search bar should pop up right side about properties of the player
+	//Search "Player" there should be an option
+	
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
 
 
 	//The first paramater is the fat and the second one is the height
 	GetCapsuleComponent()->SetCapsuleSize(100, 150);
 
-	lol = CreateDefaultSubobject<USpringArmComponent>(TEXT("lol"));
-	lol->SetupAttachment(GetCapsuleComponent());
-	lol->TargetArmLength = 0;
+	arm = CreateDefaultSubobject<USpringArmComponent>(TEXT("arm"));
+	arm->SetupAttachment(GetCapsuleComponent());
+	arm->TargetArmLength = 0;
 
-	lol->bEnableCameraLag = true;
-	lol->CameraLagSpeed = 4.0f;
-	lol->CameraLagMaxDistance = 1.0f;
-	lol->bEnableCameraRotationLag = true;
-	lol->CameraRotationLagSpeed = 8;
-	lol->CameraLagMaxTimeStep = 1;
+	arm->bEnableCameraLag = true;
+	arm->CameraLagSpeed = 4.0f;
+	arm->CameraLagMaxDistance = 1.0f;
+	arm->bEnableCameraRotationLag = true;
+	arm->CameraRotationLagSpeed = 8;
+	arm->CameraLagMaxTimeStep = 1;
 
 	cam = CreateDefaultSubobject<UCameraComponent>(TEXT("cam"));
 	cam->SetupAttachment(lol, USpringArmComponent::SocketName);
 
 	//gun
-	shit = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("shit"));
+	gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("gun"));
 
-	liz = CreateDefaultSubobject<UBoxComponent>(TEXT("liz"));
-	liz->SetupAttachment(RootComponent);
+	Collsion_component = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision_component"));
+	Collsion_component->SetupAttachment(RootComponent);
 
 	//Hands
-	sesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
-	sesh->SetupAttachment(GetCapsuleComponent());
+	hands = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("hands"));
+	hands->SetupAttachment(GetCapsuleComponent());
 
-	pen = CreateDefaultSubobject<USceneComponent>(TEXT("pen"));
-	pen->SetupAttachment(shit);
+	Bullet_go = CreateDefaultSubobject<USceneComponent>(TEXT("Bullet_go"));
+	Bullet_go->SetupAttachment(shit);
 
 	fpp = true;
 
@@ -60,8 +63,8 @@ APRO_IT_IS::APRO_IT_IS()
 // Called when the game starts or when spawned
 void APRO_IT_IS::BeginPlay()
 {
-	shit->AttachToComponent(sesh, FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("GripPoint"));
-	liz->OnComponentBeginOverlap.AddDynamic(this, &APRO_IT_IS::OnOverlapBegin);
+	gun->AttachToComponent(hands, FAttachmentTransformRules::SnapToTargetNotIncludingScale, TEXT("GripPoint"));
+	Collsion_component->OnComponentBeginOverlap.AddDynamic(this, &ACharacter_Movement::OnOverlapBegin);
 	Super::BeginPlay();
 }
 
@@ -74,16 +77,18 @@ void APRO_IT_IS::Tick(float DeltaTime)
 // Called to bind functionality to input
 void APRO_IT_IS::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
+	
+	//Go to Project Settings -> Input to configure movement
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	InputComponent->BindAction("Switch", IE_Pressed, this, &APRO_IT_IS::lpp);
-	InputComponent->BindAxis("Vertical", this, &APRO_IT_IS::Forward_Back);
-	InputComponent->BindAxis("Horizontal", this, &APRO_IT_IS::Left_Right);
-	InputComponent->BindAxis("Horizontal_View", this, &APRO_IT_IS::Horizontal_View);
-	InputComponent->BindAxis("Vertical_View", this, &APRO_IT_IS::Vertical_View);
-	InputComponent->BindAction("Fire", IE_Pressed, this, &APRO_IT_IS::fire);
+	InputComponent->BindAction("Switch", IE_Pressed, this, &ACharacter_Movement::tpp);
+	InputComponent->BindAxis("Vertical", this, &ACharacter_Movement::Forward_Back);
+	InputComponent->BindAxis("Horizontal", this, &ACharacter_Movement::Left_Right);
+	InputComponent->BindAxis("Horizontal_View", this, &ACharacter_Movement::Horizontal_View);
+	InputComponent->BindAxis("Vertical_View", this, &ACharacter_Movement::Vertical_View);
+	InputComponent->BindAction("Fire", IE_Pressed, this, &ACharacter_Movement::fire);
 }
 
-void APRO_IT_IS::lpp() {
+void APRO_IT_IS::tpp() {
 	if (fpp == false) {
 		lol->TargetArmLength = 0;
 		fpp = true;
@@ -94,62 +99,40 @@ void APRO_IT_IS::lpp() {
 	}
 }
 
-void APRO_IT_IS::Forward_Back(float value) {
+void ACharacter_Movement::Forward_Back(float value) {
 	AddMovementInput(cam->GetForwardVector(), value);
 }
 
-void APRO_IT_IS::Left_Right(float value) {
+void  ACharacter_Movement::Left_Right(float value) {
 	AddMovementInput(cam->GetRightVector(), value);
 }
 
-void APRO_IT_IS::Horizontal_View(float value) {
-	lol->AddLocalRotation(FRotator(0.0f, value, 0.0f));
-	sesh->AddLocalRotation(FRotator(0.0f, value, 0.0f));
+void  ACharacter_Movement::Horizontal_View(float value) {
+	arm->AddLocalRotation(FRotator(0.0f, value, 0.0f));
+	hands->AddLocalRotation(FRotator(0.0f, value, 0.0f));
 }
 
-void APRO_IT_IS::Vertical_View(float value) {
+void  ACharacter_Movement::Vertical_View(float value) {
 	if (value) {
-		float temp = lol->GetRelativeRotation().Pitch + value; // The X axis
+		float temp = arm->GetRelativeRotation().Pitch + value; // The X axis
 		if (temp < 25 && temp > -65) { // Not making the head to up by 25 and not to down by -65
-			lol->AddLocalRotation(FRotator(value, 0, 0)); // If conditions meet then add the value
-			sesh->AddLocalRotation(FRotator(value, 0, 0));
+			arm->AddLocalRotation(FRotator(value, 0, 0)); // If conditions meet then add the value
+			hands->AddLocalRotation(FRotator(value, 0, 0));
 		}
 	}
 }
 
-void APRO_IT_IS::fire(){
+void ACharacter_Movement::fire(){
 
-	start = pen->GetComponentLocation();
-	axis_of_the_worlds = pen->GetComponentRotation();
+	start = Bullet_go->GetComponentLocation();
+	axis_of_the_worlds = Bullet_go->GetComponentRotation();
 
 	//The FActorSpawnParamter passed to the spawn actor function.
 	spawn.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding;
 
 	if(GetWorld()) {
-		GetWorld()->SpawnActor<ABullet_man>(bullf , start , axis_of_the_worlds , spawn);
+		GetWorld()->SpawnActor<ABullet_man>(bullet_class_mesh , start , axis_of_the_worlds , spawn);
 	}
 
-	UGameplayStatics::PlaySoundAtLocation(this, hell, pen->GetComponentLocation());
-}
-
-
-void APRO_IT_IS::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const  FHitResult& SweepResult) {
-	FString name = OtherComp->GetName();
-
-	if(name.Contains("me_get_damaged")){
-		health = health - 20;
-	}
-
-	if(health < 0 || health  == 0){
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, FString::Printf(TEXT("You Lost!")));
-
-		AMyGameModeBase* lood = Cast<AMyGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
-
-
-		if(lood){
-			lood->Restart_Gameplay(false);
-		}
-
-		Destroy();
-	}
+	UGameplayStatics::PlaySoundAtLocation(this, Sound_Play , pen->GetComponentLocation());
 }
